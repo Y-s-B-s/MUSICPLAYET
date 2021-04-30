dd6e512b2468f9748d232ba7c031dab1fcd32d65
Itz-Fork <77770753+Itz-fork@users.noreply.github.com>
2021-04-30 21:24:27 +0530

    c+

--- callsmusic/handlers/admins/resume.py
+++ callsmusic/handlers/admins/resume.py
@@ -21,7 +21,7 @@ from callsmusic.helpers.filters import command
 from callsmusic.helpers.filters import other_filters
 
 
-@Client.on_message(command('resume') & other_filters)
+@Client.on_message(command('cresume') & other_filters)
 @errors
 @authorized_users_only
 async def _(_, message: Message):
